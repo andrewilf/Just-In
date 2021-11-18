bd3bbfa1a3a46b1c9cae4ce0491bee82e9012cf2
andrewfaulkner <andrewfau1kn3r@gmail.com>
2021-11-19 04:33:07 +0800

    added ability to see if streamer is online twitch and get redirected to stream

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -23,8 +23,9 @@ const mapStateToProps = (state) => {
 
 function Sidebar(props) {
   const apiKey = JSON.parse(localStorage.getItem('justinkeys'))
-  console.log(apiKey)
+  //console.log(apiKey)
   const [showNavExternal, setShowNavExternal] = useState(true);
+  const [twitchStatus, setTwitchStatus] = useState(false)
 
   const headers = {
     "Authorization": `Bearer ${apiKey.twitch_bearer}`,
@@ -37,16 +38,22 @@ function Sidebar(props) {
   }
 
   async function fetchDataTwitch(userId) {
-    const URL = `https://shrill-cloud-4f83.wenjie-teo.workers.dev/helix/search/channels?query=${userId}&ga_proxy=api.twitch.tv`
-    console.log(headers)
+    const URL = `https://shrill-cloud-4f83.wenjie-teo.workers.dev/helix/search/channels?query=${userId}&first=3&ga_proxy=api.twitch.tv`
+    //console.log(headers)
     try {
       const response = await fetch(URL, options);
       const data = await response.json();
       console.log(data);
-      return data
-      //const arrData = await data.data.map((element) => ({ mediaType: "Twitch", id: element.id, created_at: element.created_at }))
-      //return arrData.filter(element =>
-      //  (!(element.created_at)))
+      //return data
+      const arrData = await data.data.map((element) => {
+        //console.log(element)
+        if (element.broadcaster_login === userId) {
+          return { streamer: userId, status: element.is_live }
+        }
+        return false
+      }).filter(Boolean)
+      console.log(arrData[0])
+      return arrData[0]
     } catch (err) {
       console.log(err)
       return false
@@ -57,16 +64,24 @@ function Sidebar(props) {
 
   const Persons = Object.keys(props.data[props.currentProfile]).map((element) => {
     const currentPerson = props.data[props.currentProfile][element]
+    const urlTwitch = `https://www.twitch.tv/${currentPerson.twitch_id}`
+    console.log(twitchStatus[currentPerson.twitch_id])
     return (
       <>
         <div style={{ display: "flex", cursor: "pointer" }} onClick={() => { window.open(`https://twitter.com/${currentPerson.twitter_name}`) }}>
           <img src={currentPerson.image_url} alt={currentPerson.twitter_name} style={{ borderRadius: "50%" }} />
-          <h5 className='text-white h4' >{currentPerson.twitter_name}</h5>
-        </div>
-        <div>
-          {(currentPerson.twitch_stream ? <span className='text-muted'>Twitch: Offline</span> : <span className='text-muted'>No streams available</span>)}
+          < h5 className='text-white h4' > {currentPerson.twitter_name}</h5 >
+        </div >
+        <div style={{width: "70px"}}>
+          {(currentPerson.twitch_stream ? <span className='text-muted'>Twitch: {
+            (twitchStatus[currentPerson.twitch_id] ? <div>
+               <a href={urlTwitch} target="_blank"> Online   </a> 
+               <i className="fas fa-redo fa-lg  fa-spin " style={{color: "#4fbc15"}}></i>
+               </div>: "Offline")
+          }</span> : <span className='text-muted'>No streams available</span>)}
+          </div>
           {(currentPerson.youtube_stream ? <span style={{ display: "block" }} className='text-muted'>  YouTube: Offline</span> : <div></div>)}
-        </div>
+        
       </>
     )
 
@@ -74,7 +89,13 @@ function Sidebar(props) {
 
   useEffect(async () => {
     console.log("running twitch update")
-    const twitchdata = await fetchDataTwitch("serral")
+    //fetchDataTwitch("serral")
+    const twitchdata = await Promise.all(
+      Object.keys(props.data[props.currentProfile]).map((element) => props.data[props.currentProfile][element])
+        .map((element) => (element["twitch_stream"] ? fetchDataTwitch(element["twitch_id"]) : false)).flat(1)
+    )
+    setTwitchStatus(twitchdata.reduce((a, v) => ({ ...a, [v.streamer]: v.status }), {}))
+    console.log(twitchdata.reduce((a, v) => ({ ...a, [v.streamer]: v.status }), {}))
   }, [props.currentProfile])
 
   return (
